d9f243b34acb5f369012fd080c6ab721796a1488
mizza-cz <mizza.cz666@gmail.com>
2024-08-13 10:25:56 +0200

    add hp

---
+++ src/components/matches-section/matches-section.js
@@ -0,0 +1,11 @@
+$(".matches-section__slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: false,
+  fade: true,
+  cssEase: "linear",
+  prevArrow:
+    '<button class="matches__btn matches__btnprev"><img src="images/ico/arrow-left.svg" loading="lazy" alt="" /></button> ',
+  nextArrow:
+    ' <button class="matches__btn  matches__btnnext"><img src="images/ico/arrow-right.svg" loading="lazy" alt="" /></button>',
+});
