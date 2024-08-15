9a5822a45ac5c59bcd3f3ba357ad822e72e4294a
mizza-cz <mizza.cz666@gmail.com>
2024-08-15 07:57:49 +0200

    add slider/archive fix

--- src/components/archive-section/archive-section.js
+++ src/components/archive-section/archive-section.js
@@ -9,35 +9,91 @@ $(".archive-section__wrapper").slick({
   autoplay: true,
 });
 
-const asideProductsContainer = document.querySelector(".archiveItems");
+/*!
+ * fetchHTML.js
+ */
+(function () {
+  const fetchHTMLButtons = document.querySelectorAll(".js-fetchHTML");
+  let fetchCounter = 0;
 
-if (asideProductsContainer) {
-  const products = Array.from(document.querySelectorAll(".archiveItem"));
-  const buttonMore = document.querySelector(".btnMore");
-  let visibleCount = 4;
+  const fetchData = (ev) => {
+    ev.preventDefault();
 
-  function showProducts() {
-    products.forEach((product, index) => {
-      if (index < visibleCount) {
-        product.classList.add("visible");
-      } else {
-        product.classList.remove("visible");
-      }
+    const fetchButton = ev.target;
+    let {
+      fetchUrl,
+      fetchContainer,
+      fetchLimit,
+      text,
+      textFetching,
+      textError,
+    } = fetchButton.dataset;
+
+    const url = new URL(window.location.href);
+
+    if (url.searchParams.has("page")) {
+      fetchCounter = parseInt(url.searchParams.get("page"));
+    }
+
+    fetchCounter++;
+
+    url.searchParams.set("page", fetchCounter);
+    window.history.pushState({}, "", url);
+    const urlParams = window.location.search;
+
+    fetchUrl = fetchUrl + urlParams;
+
+    if (fetchCounter >= fetchLimit) {
+      fetchButton.classList.add("d-none");
+    }
+
+    const req = new Request(fetchUrl, {
+      method: "GET",
     });
 
-    if (visibleCount >= products.length) {
-      buttonMore.style.display = "none";
-    } else {
-      buttonMore.style.display = "flex";
-    }
+    fetchButton.innerText = textFetching;
+    fetchButton.disabled = true;
+    fetchButton.style.pointerEvents = "none";
+
+    const container = document.querySelector(fetchContainer);
+
+    const enableFetchButton = () => {
+      fetchButton.innerText = text;
+      fetchButton.disabled = false;
+      fetchButton.style.pointerEvents = "all";
+    };
+
+    const checkError = (response) => {
+      if (response.status >= 200 && response.status <= 299) {
+        return response.text();
+      } else {
+        throw Error(response.status);
+      }
+    };
+
+    const handleError = (error) => {
+      console.warn("Fetch failed:", error.message);
+      container &&
+        container.insertAdjacentHTML(
+          "afterend",
+          `<p class="u-ta-center u-fs-big u-fw-bold u-c-red u-my-24">${textError}</p>`
+        );
+      enableFetchButton();
+    };
+
+    const handleData = (HTML) => {
+      const frag = document.createRange().createContextualFragment(HTML);
+      container && container.appendChild(frag);
+      enableFetchButton();
+    };
+
+    fetch(req).then(checkError).then(handleData).catch(handleError);
+    // .finally();
+  };
+
+  if (fetchHTMLButtons.length) {
+    fetchHTMLButtons.forEach((fetchBtn) => {
+      fetchBtn.addEventListener("click", fetchData);
+    });
   }
-
-  function loadMoreProducts() {
-    visibleCount += 4;
-    showProducts();
-  }
-
-  buttonMore.addEventListener("click", loadMoreProducts);
-
-  showProducts();
-}
+})();
--- src/components/matches-section/matches-section.js
+++ src/components/matches-section/matches-section.js
@@ -1,11 +1,22 @@
-$(".matches-section__slider").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  infinite: false,
-  fade: true,
-  cssEase: "linear",
-  prevArrow:
-    '<button class="matches__btn matches__btnprev"><img src="/build/images/ico/arrow-left.svg" loading="lazy" alt="" /></button> ',
-  nextArrow:
-    ' <button class="matches__btn  matches__btnnext"><img src="/build/images/ico/arrow-right.svg" loading="lazy" alt="" /></button>',
+$(document).ready(function () {
+  // Для каждого слайдера с классом .matches-section__slider
+  $(".matches-section__slider").each(function () {
+    var actualSlide = parseInt($(this).attr("data-actual"), 10);
+
+    var initialSlide = actualSlide ? actualSlide - 1 : 0;
+
+    $(this).slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: false,
+      fade: true,
+      cssEase: "linear",
+      swipeToSlide: true,
+      initialSlide: initialSlide,
+      prevArrow:
+        '<button class="matches__btn matches__btnprev"><img src="/build/images/ico/arrow-left.svg" loading="lazy" alt="" /></button>',
+      nextArrow:
+        '<button class="matches__btn matches__btnnext"><img src="/build/images/ico/arrow-right.svg" loading="lazy" alt="" /></button>',
+    });
+  });
 });
